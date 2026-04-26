29b9089e88f65aae1eedb46a9565e2e27af587d0
Eduardo Fabrizio <efabriziosiqueira@gmail.com>
2026-04-26 12:27:50 +0000

    task2 done

--- catkin_ws/src/task2/src/publisher_node2.cpp
+++ catkin_ws/src/task2/src/publisher_node2.cpp
@@ -1,30 +1,41 @@
 #include <ros/ros.h> 
 #include <std_msgs/Int32.h>
-#include <geometry_msgs/Point.h>
+#include <task2/coords_2d.h>
+#include <task2/coords_2d_array.h>
 #include <iostream>
 
 class NodePublicador{
 private:
     ros::NodeHandle nh_;
     ros::Publisher pub_coordenadas;
+    task2::coords_2d_array coordenadas;
 public:
     NodePublicador(ros::NodeHandle* nh){ //construtorzinho
         nh_ = *nh;
-        pub_coordenadas = nh_.advertise<geometry_msgs::Point>("coordenadas", 10);
+        pub_coordenadas = nh_.advertise<task2::coords_2d_array>("coordenadas", 10);
+        carregarParametros();
         ROS_INFO("Classe do Publisher iniciada!");
     }
+    void carregarParametros(){
+      XmlRpc::XmlRpcValue lista; //para ler yaml, tem que ser XmlRpcValue
+        if (nh_.getParam("points", lista)) { //condiçao vai atras da lista "points"
+        for(int i=0; i < lista.size(); i++) {
+            XmlRpc::XmlRpcValue ponto = lista[i]; //cada ponto é um XmlRpcValue
+            task2::coords_2d ponto_msg; //cria a mensagem do tipo coords_2d
+            ponto_msg.x = static_cast<float>(static_cast<int>(ponto[0])); //atribui o valor de x do ponto à mensagem
+            ponto_msg.y = static_cast<float>(static_cast<int>(ponto[1])); //atribui o valor de y do ponto à mensagem
+            coordenadas.coordinates.push_back(ponto_msg); //adicion
+        }
+        ROS_INFO("Coordenadas carregadas com sucesso!");
+    }}
     void publicaCiclicamente(){
-        ros::Rate loop_rate(1); // 1 Hz
-        while (ros::ok()){
-            geometry_msgs::Point ponto;
-            ponto.x = 2.6;
-            ponto.y = 3.4;
-            pub_coordenadas.publish(ponto);
-            ROS_INFO("Publicado: (%.2f, %.2f)", ponto.x, ponto.y);
+        ros::Rate loop_rate(1); //publica a cada 1 segundo
+        while (ros::ok()) {
+            pub_coordenadas.publish(coordenadas); //publica a mensagem com as coordenadas
+            ROS_INFO("%d Coordenadas publicadas!", static_cast<int>(coordenadas.coordinates.size()));
             ros::spinOnce();
             loop_rate.sleep();
-        }
-    }
+    }}
 };
 int main(int argc, char **argv){
     ros::init(argc, argv, "number_publisher2");
--- catkin_ws/src/task2/src/subscriber_node2.cpp
+++ catkin_ws/src/task2/src/subscriber_node2.cpp
@@ -1,16 +1,70 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Point.h>
 #include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <task2/coords_2d.h>
+#include <task2/coords_2d_array.h>
 
 class NodeMarcadores {
 private:
     ros::NodeHandle nh_;
+    ros::Subscriber sub_coordenadas;
+    ros::Publisher pub_marcadores;
 
 public:
     NodeMarcadores(ros::NodeHandle* nh) {
         nh_ = *nh;
+
+        pub_marcadores = nh_.advertise<visualization_msgs::MarkerArray>("marcadores", 10);
+        sub_coordenadas = nh_.subscribe("coordenadas", 10, &NodeMarcadores::callbackCoordenadas, this);
         ROS_INFO("Classe do Subscriber/Marcadores iniciada!");
     }
+
+    void callbackCoordenadas(const task2::coords_2d_array::ConstPtr& msg) {
+        
+        visualization_msgs::MarkerArray array_marcadores; // A "caixa" que vai pro RViz
+
+        // Um loop para transformar cada coords_2d em uma Esfera 3D
+        for (size_t i = 0; i < msg->coordinates.size(); i++) {
+            
+            visualization_msgs::Marker marcador;
+            
+            // Configs para o RViz
+            marcador.header.frame_id = "map";  // O nome do nosso 3D
+            marcador.header.stamp = ros::Time::now();
+            marcador.ns = "fst_pontos";        // Um nome/namespace
+            marcador.id = i;                   // ID único usando o 'i' do loop (0 a 102)
+            
+            // Tipo e Ação
+            marcador.type = visualization_msgs::Marker::SPHERE; // ESFERAS
+            marcador.action = visualization_msgs::Marker::ADD;  // ADICIONAMOS
+
+            // Posição no mundo (Copiamos da SUA mensagem customizada)
+            marcador.pose.position.x = msg->coordinates[i].x;
+            marcador.pose.position.y = msg->coordinates[i].y;
+            marcador.pose.position.z = 0.0; // Pontos 2D, então coloquei Z sempre a zero
+            
+            marcador.pose.orientation.w = 1.0; // Rotação padrão (n sei se é necessario)
+
+            // Escala (Como as coordenadas sao mtas tamanho 15 fica visível)
+            marcador.scale.x = 15.0;
+            marcador.scale.y = 15.0;
+            marcador.scale.z = 15.0;
+
+            // Cor RGBA (Red, Green, Blue, Alpha) - esferas Azuis!
+            marcador.color.r = 0.0f;
+            marcador.color.g = 0.5f;
+            marcador.color.b = 1.0f;
+            marcador.color.a = 1.0f; // Alpha = 1.0 significa 100% opaco
+
+            // Esfera pronta dentro da caixa
+            array_marcadores.markers.push_back(marcador);
+        }
+
+        // Publica a caixa para o RViz
+        pub_marcadores.publish(array_marcadores);
+    }
+
 }; 
 
 int main(int argc, char **argv) {
